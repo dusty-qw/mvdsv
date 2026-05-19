5d04215e5f5759e01bdb69bbadae57d11c9034ff
dusty-qw <71472647+dusty-qw@users.noreply.github.com>
2026-05-18 18:01:46 -0700

    SPRAY DECALS: initial support

--- src/sv_init.c
+++ src/sv_init.c
@@ -359,6 +359,9 @@ void SV_SpawnServer(char *mapname, qbool devmap, char* entityfile, qbool loading
 	// NOTE: this also set sv.mvdrecording to false, so calling SV_MVD_Record() at end of function
 	memset (&sv, 0, sizeof(sv));
 	sv.max_edicts = MAX_EDICTS_SAFE;
+#ifdef MVD_PEXT1_SPRAYS
+	SV_SpraysNewMap();
+#endif
 
 	sv.datagram.maxsize = sizeof(sv.datagram_buf);
 	sv.datagram.data = sv.datagram_buf;
@@ -460,6 +463,18 @@ void SV_SpawnServer(char *mapname, qbool devmap, char* entityfile, qbool loading
 		}
 	}
 #endif
+#ifdef MVD_PEXT1_SPRAYS
+	{
+		extern cvar_t sv_sprays;
+
+		if (sv_sprays.value) {
+			svs.mvdprotocolextension1 |= MVD_PEXT1_SPRAYS;
+		}
+		else {
+			svs.mvdprotocolextension1 &= ~MVD_PEXT1_SPRAYS;
+		}
+	}
+#endif
 
 	// find optional QC-exported functions.
 	// we have it here, so we set it to NULL in case of PR2 progs.
@@ -474,6 +489,8 @@ void SV_SpawnServer(char *mapname, qbool devmap, char* entityfile, qbool loading
 	GE_ClientCommand = ED_FindFunctionOffset ("GE_ClientCommand");
 	GE_PausedTic = ED_FindFunctionOffset ("GE_PausedTic");
 	GE_ShouldPause = ED_FindFunctionOffset ("GE_ShouldPause");
+	GE_CanSpray = ED_FindFunctionOffset ("GE_CanSpray");
+	GE_SprayPlaced = ED_FindFunctionOffset ("GE_SprayPlaced");
 
 	// leave slots at start for clients only
 	sv.num_edicts = MAX_CLIENTS+1;
